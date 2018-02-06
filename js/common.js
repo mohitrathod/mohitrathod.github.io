526b80961d481d478bc9bc0f9d67a58136644aee
mohitrathod <er.mohit.rathod@gmail.com>
2018-02-06 19:59:45 +0530

    updates

--- js/common.js
+++ js/common.js
@@ -4,5 +4,4 @@ $(document).ready(function () {
   var path = window.location.pathname.substring(1);
   path = '/' + path.trim();
   $('.scrollmenu>a[href="' + path + '"]').addClass('active');
-  alert(path);
 });
